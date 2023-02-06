fedb5b7e5a0eb198c3d0faea464b253ed78dbdeb
seamew <seamew121@gmail.com>
2023-02-06 19:42:29 +0800

    2023-02-06

--- spring-rabbitmq-producer/src/test/java/com/seamew/producer/MessageProducerTest.java
+++ spring-rabbitmq-producer/src/test/java/com/seamew/producer/MessageProducerTest.java
@@ -1,9 +1,12 @@
 package com.seamew.producer;
 
+import com.seamew.config.QueueConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,16 +14,39 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 // 指明 spring 配置文件位置
 @ContextConfiguration("classpath:spring-rabbitmq.xml")
+@Slf4j(topic = "r.MessageProducerTest")
 public class MessageProducerTest
 {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Value("${rabbitmq.queue.simpleMode.queueName}")
+    public String simpleModeQueueName;
+
+    @Value("${rabbitmq.queue.workQueueMode.queueName}")
+    public String workQueueModeQueueName;
+
     @Test
     public void testSimpleMessagePublish()
     {
         // 简单模式测试
+        // 简单模式下消息会被发送到默认交换机，并以队列名称作为路由键将消息转发到相应的队列中
+        String msg = "Simple mode";
+        log.debug("Sending message [{}]", msg);
+        rabbitTemplate.convertAndSend("", simpleModeQueueName, msg);
+        log.debug("Complete");
+    }
 
+    @Test
+    public void testTaskPublish()
+    {
+        // 工作队列模式测试
+        String basicMessage = "WorkQueue mode";
+        for (int i = 0; i < 10; i++) {
+            String message = basicMessage + i;
+            log.debug("Sending message [{}]", message);
+            rabbitTemplate.convertAndSend("", "spring-workqueue-queue", "");
+        }
     }
 
     @Test
